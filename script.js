d2355f2b25301a4be973a938dc2b1d2a963acf5e
azlaan2605 <azzuazlaan@gmail.com>
2021-08-21 09:25:56 +0530

    dom

---
+++ script.js
@@ -0,0 +1,19 @@
+let myv = document.getElementById("do")
+console.log(myv);
+
+console.log(myv.firstChild);
+
+console.log(myv.lastChild);
+
+console.log(myv.children);
+
+console.log(myv.childNodes);
+  
+console.log(myv.firstElementChild);
+
+console.log(myv.lastElementChild);
+
+let var2 = getElementById("hello")
+console.log(var2.nextElementSibling);
+
+console.log(var2.previousElementSibling);
